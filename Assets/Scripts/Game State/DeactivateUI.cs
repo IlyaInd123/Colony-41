11888bec096867d8136bbe9189408941cd1e4cd4
HassanAbusalih <hassan.abusalih@student.saedubai.com>
2023-12-23 05:36:01 +0400

    Fixed deactivating UI

--- Assets/Scripts/Game State/DeactivateUI.cs
+++ Assets/Scripts/Game State/DeactivateUI.cs
@@ -3,9 +3,24 @@
 public class DeactivateUI : MonoBehaviour
 {
     [SerializeField] GameObject panel;
+    bool panelClosed = false;
+
+    private void Update()
+    {
+        if (!panelClosed)
+        {
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
 
     public void ClosePanel()
     {
         panel.SetActive(false);
+        panelClosed = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1;
     }
 }
